81fc3f67d224a0deec90c1478ce428ae3cbca4ef
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-03-08 15:43:14 -0800

    dynamically adds the temperature to each day in the forecast

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,8 +10,38 @@ const getWeatherData = cityName => {
     fetch(apiUrl).then(function(response){
         // reformat into json
         response.json().then(function(data) {
-            // log resulting data
-            console.log(data.list[0].weather);
+            console.log(data.list);
+            let counter =1;
+            for(let i=0; i < data.list.length; i+=8){
+                // current day in the forecast
+                const currentDay = data.list[i];
+                const $forecastDayEl = $("#forecast-day-"+ counter);
+
+                // grab header
+                const $forecastDayHeaderEl = $(".date");
+                console.log($forecastDayHeaderEl);
+
+                // grab icon & create element
+                const weatherIconId = currentDay.weather[0].icon;
+                let iconSrc = `http://openweathermap.org/img/w/${weatherIconId}.png`
+                let $iconImg = $("<img>").attr("src", iconSrc).addClass("icon");
+
+                // grab temperature & create p element
+                const tempKelvin = currentDay.main.temp;
+
+                const fahrenheitConverter = kelvin => (((kelvin - 273.15)*9)/5)+32;
+                const tempFahrenheit = Math.floor(fahrenheitConverter(tempKelvin));
+                let $tempEl = $("<p>").text(`Temp: ${tempFahrenheit}`);
+
+                // grab humidity & create p element
+
+                // append to container
+                $forecastDayEl.append($iconImg);
+                $forecastDayEl.append($tempEl);
+
+                // add to counter
+                counter++;
+            }
         });
     });
     displayWeatherData(cityName);
