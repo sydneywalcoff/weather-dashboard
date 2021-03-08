803f219e1ad3f312715754b881d4cd1ad5cb8899
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-03-08 09:53:05 -0800

    add fetch and api key info

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,13 @@
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 // api key
 // 7740f742ec7905224fc725aeef79fcd9
+
+
+const getWeatherData = cityName => {
+    let apiKey = '7740f742ec7905224fc725aeef79fcd9';
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+    
+    fetch(apiUrl);
+
+};
+getWeatherData('detroit')
