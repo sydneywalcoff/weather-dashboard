35e68f337f51c4947e7faf1bc1aaa27264c4fc73
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-03-08 16:52:01 -0800

    dynamically add weather api data to the current weather endpoint

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,44 @@ const $featuredH2 = $('h2');
 const featuredTemp = $('.featured-temperature').text();
 
 const getWeatherData = cityName => {
+    const degreeSign = '\u00B0';
     let apiKey = '7740f742ec7905224fc725aeef79fcd9';
-    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
-    // make api request
-    fetch(apiUrl).then(function(response){
+    let forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+    let currentApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+    fetch(currentApiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+
+            // grab temperature and append to #featured-temp
+            const temp = Math.floor(data.main.temp);
+            $('#featured-temp').append(temp + ' ' + degreeSign + 'F');
+
+            // grab humidity and append to #featured-humidity
+            const humidity = Math.floor(data.main.humidity);
+            $('#featured-humidity').append(humidity + '%');
+
+            // grab wind speed and append to #featured-wind-speed
+            const windSpeed = Math.floor(data.wind.speed);
+            $('#featured-wind-speed').append(windSpeed + ' ' + 'MPH');
+
+            // grab uv index and append to #featured-uv-index
+
+            // get icon and get date
+            const weatherIconId = data.weather[0].icon;
+            const iconSrc = `http://openweathermap.org/img/w/${weatherIconId}.png`;
+            const $iconImg = $("<img>").attr("src", iconSrc).addClass("icon-sm");
+
+            // append to $featuredH2
+            $featuredH2.append($iconImg)
+
+
+        });
+    });
+    
+    // make forecast api request
+    fetch(forecastApiUrl).then(function(response){
         // reformat into json
         response.json().then(function(data) {
-            console.log(data.list);
             let counter =1;
             for(let i=0; i < data.list.length; i+=8){
                 // current day in the forecast
@@ -19,7 +50,6 @@ const getWeatherData = cityName => {
 
                 // grab header
                 const $forecastDayHeaderEl = $(".date");
-                console.log($forecastDayHeaderEl);
 
                 // grab icon & create element
                 const weatherIconId = currentDay.weather[0].icon;
@@ -27,7 +57,6 @@ const getWeatherData = cityName => {
                 const $iconImg = $("<img>").attr("src", iconSrc).addClass("icon");
 
                 // grab temperature & create p element
-                const degreeSign = '\u00B0';
                 const tempKelvin = currentDay.main.temp;
 
                 const fahrenheitConverter = kelvin => (((kelvin - 273.15)*9)/5)+32;
